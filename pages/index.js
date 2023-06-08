233cf676247014800840e8b63eef49ed1d98195e
JC <nunez.jeycee@gmail.com>
2023-06-08 11:41:40 +0800

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
   useEffect(() => {
     checkCounter()
     checkWin()
-  },[counter])
+  },[counter,checkCounter,checkWin])
 
   async function checkCounter() {
     if(counter === 2){
