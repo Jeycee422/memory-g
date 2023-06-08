db6c228c39481e01c0b2d179214d863d6bd4e79d
JC <nunez.jeycee@gmail.com>
2023-06-08 22:28:59 +0800

    update

--- pages/components/Tiles.js
+++ pages/components/Tiles.js
@@ -14,7 +14,7 @@ export default function Tiles(props){
                 ? <Tile name={props.name} color={props.color} />
             
                 :<div onClick={props.onClick} className="bg-[#053876] h-12 w-12 sm:w-[72px] sm:h-[72px] border border-none rounded-lg sm:rounded-xl cursor-pointer">
-                    <div className="bg-[#2281f6] h-[36px] sm:h-16 sm:text-4xl border border-none rounded-lg sm:rounded-xl justify-center items-center flex text-2xl" style={monts}>
+                    <div className="bg-[#2281f6] h-[42px] sm:h-16 sm:text-4xl border border-none rounded-lg sm:rounded-xl justify-center items-center flex text-2xl" style={monts}>
                         {/* <Question className="w-8 h-8" /> */}?
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -219,16 +219,16 @@ export default function Home() {
     <div className="no-select bg-[#0d131c] w-full sm:w-screen h-screen text-white text-4xl justify-center items-center flex flex-col">
       {levelSelect
         ?
-          <div className='flex flex-col space-y-10' style={montserrat.style}>
-            <div className='text-3xl sm:text-5xl space-x-4 flex flex-row justify-center items-center'><Brainsvg className='w-10 h-10 sm:h-14 sm:w-14' /><div>Memory Game</div></div>
-            <button className='text-xl sm:text-3xl' onClick={() => {handleLevel(4)}}>
+          <div className='flex flex-col space-y-10 items-center' style={montserrat.style}>
+            <div className='text-3xl sm:text-5xl space-x-4 flex flex-row justify-center items-center mb-6'><Brainsvg className='w-10 h-10 sm:h-14 sm:w-14' /><div>Memory Game</div></div>
+            <button className='text-xl sm:text-3xl text-[#d97706] hover:text-[#f59e0b] text-left w-36 sm:w-60' onClick={() => {handleLevel(4)}}>
               <div>Easy</div>
             </button>
 
-            <button className='text-xl sm:text-3xl' onClick={() => {handleLevel(6)}}>
+            <button className='text-xl sm:text-3xl text-[#b91c1c] hover:text-[#dc2626] text-left w-36 sm:w-60' onClick={() => {handleLevel(6)}}>
               <div>Hard</div>
             </button>
-            <button className='text-xl sm:text-3xl' onClick={() => {handleLevel(8)}}>
+            <button className='text-xl sm:text-3xl text-[#881337] hover:text-[#be123c] text-left w-36 sm:w-60' onClick={() => {handleLevel(8)}}>
               <div>Insane</div>
             </button>
           </div>
@@ -247,13 +247,13 @@ export default function Home() {
                 {board}
               </div> : null
             }
-            <div className='mt-4 w-11/12 h-8 sm:h-12 flex justify-center items-center space-x-4'>
-              {highScore ? <div className='text-[#fbbf24] text-lg w-24 capitalize text-center' style={montserrat.style}>best: {highScore}</div> : null}
-              <button onClick={showPlay ? play : showScore ? null : playAgain} className='capitalize w-3/12 sm:w-4/12 h-full bg-red-500 text-[10px] sm:text-base flex justify-center items-center border border-none rounded-md cursor-pointer' style={montserrat.style}><div style={{userSelect: 'none'}}>{showPlay ? 'Play' : showScore ? `Score: ${score}` : 'Play Again'}</div>
+            <div className='mt-4 w-11/12 h-8 sm:h-12 flex justify-center items-center'>
+              {highScore ? <div className='text-[#fefce8] text-sm sm:text-lg w-24 capitalize text-center sm:mr-4 flex flex-row items-center' style={montserrat.style}>best: <div className='text-[#fde047] text-lg sm:text-2xl ml-2'>{highScore}</div></div> : null}
+              <button onClick={showPlay ? play : showScore ? null : playAgain} className='mr-2 sm:mr-4 capitalize w-28 sm:w-40 h-full bg-red-500 text-xs sm:text-base flex justify-center items-center border border-none rounded-md cursor-pointer' style={montserrat.style}><div style={{userSelect: 'none'}}>{showPlay ? 'Play' : showScore ? `Score: ${score}` : 'Play Again'}</div>
               </button>
               
               <div onClick={restart}>
-                <Resetsvg className="w-4 sm:w-7 cursor-pointer" />
+                <Resetsvg className="w-5 sm:w-7 cursor-pointer" />
               </div>
             </div>
           </div>
