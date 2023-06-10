cfab0bee12787bd9989919d6bd64fcbf90d827b3
JC <nunez.jeycee@gmail.com>
2023-06-10 11:02:54 +0800

    update typo sorry

--- pages/index.js
+++ pages/index.js
@@ -114,7 +114,7 @@ export default function Home() {
     await sleep(250)
     const diff = level === 4 ? 16 : level === 6 ? 36 : level === 8 ? 64 : null
     shuffle(diff)
-    setDisabled(true)
+    setDisabled(false)
     setGameOver(false)
     setShowScore(true)
     setScore(0)
